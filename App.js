f30485e2767e27846091b2f6348595570be6e9a2
Albert <20127370@student.hcmus.edu.vn>
2022-05-13 13:44:03 +0700

    reactnative pt. 1

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
+//import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, SafeAreaView } from 'react-native';
+
+const Cat = () => {
+  return (
+    <View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="Type your name"
+      />
+    </View>
+  );
+}
 
 export default function App() {
+  console.log("App executed");
+  console.log(require("./assets/favicon.png"));
+
+  const handlePress = () => console.log("Text pressed");
+  const getFullName = (fname, sname, tname) => {
+    return fname + " " + sname + " " + tname;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>HN của QT xinh đẹp</Text>
+      <Text numberOfLines={3} onPress={() => console.log("Text clicked")}>
+        This is my first project using {getFullName("React", "Native", "JavaScript")}- Albert.
+      </Text>
+      <Image source = {require("./assets/favicon.png")}/>
+      <Image 
+        source = {{
+          width: 200, height: 300,
+          uri: "https://picsum.photos/200/300"
+        }}
+      />
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="Type here"
+      />
+
+      {Cat()}
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
